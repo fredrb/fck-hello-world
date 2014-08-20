57289d28fbed212c14c5f26d3a26980e55198d80
fredrb <fred-rb@hotmail.com>
2014-08-20 19:02:30 -0300

    Aug 20th - 2014 - 0.0.1 - Front End - Update in Jquery

--- public/javascript/anim.jq.js
+++ public/javascript/anim.jq.js
@@ -14,4 +14,35 @@ $(document).ready(function(){
          })
       }
    );
+
+   /*
+   * @TODO - FIX BUG
+   * > .list td a
+   * > problem when hover $("td a") then $("td").
+   * > background color of $("a") is lost.
+   * */
+
+   $(".list tr :nth-child(1)").hover(
+      function(){
+            if( !$(this).hasClass("caption") ) {
+               $(this).css({
+                  "background-color": "rgb(215,215,213)"
+               });
+               $(this).children("a").css({
+                  "background-color": "rgb(215,215,213)"
+               });
+            }
+         },
+         function(){
+            if( !$(this).hasClass("caption") ) {
+               $(this).css({
+                  "background-color": "rgb(229,229,225)"
+               });
+               $(this).children("a").css({
+                  "background-color": "rgb(229,229,225)"
+               });
+            }
+         }
+
+   );
 });
